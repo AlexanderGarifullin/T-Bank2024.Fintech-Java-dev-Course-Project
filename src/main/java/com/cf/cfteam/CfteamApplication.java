02f4db7aaf01305b757e4a334ce17cacff34c8a6
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-09-29 20:18:40 +0500

    Initial commit

---
+++ src/main/java/com/cf/cfteam/CfteamApplication.java
@@ -0,0 +1,13 @@
+package com.cf.cfteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cf/cfteam/CfteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.cfteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
