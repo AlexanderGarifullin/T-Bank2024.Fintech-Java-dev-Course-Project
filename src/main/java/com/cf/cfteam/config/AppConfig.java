058c385d1aa4150cad700d6582e2e1e1eec1452f
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-12 20:10:06 +0500

    dev/security/ Добавил бины для security

--- src/main/java/com/cf/cfteam/config/AppConfig.java
+++ src/main/java/com/cf/cfteam/config/AppConfig.java
@@ -1,19 +1,43 @@
 package com.cf.cfteam.config;
 
+import com.cf.cfteam.services.security.MyUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestClient;
 
 @Configuration
-
+@RequiredArgsConstructor
 public class AppConfig {
+
+    private final MyUserDetailsService myUserDetailsService;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return myUserDetailsService;
     }
 
     @Bean
