204072fcefea55a466f555c0fc0dc5baaeef9687
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-13 03:46:30 +0500

    dev/security/ Написал unit тест для TokenService

---
+++ src/test/java/com/cf/cfteam/services/security/TokenServiceTest.java
@@ -0,0 +1,71 @@
+package com.cf.cfteam.services.security;
+
+
+import com.cf.cfteam.exceptions.security.TokenNotFoundException;
+import com.cf.cfteam.models.entities.security.Token;
+import com.cf.cfteam.repositories.jpa.security.TokenRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+class TokenServiceTest {
+
+    @InjectMocks
+    private TokenService tokenService;
+
+    @Mock
+    private TokenRepository tokenRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void isTokenRevoked_shouldReturnTrue_whenTokenIsRevoked() {
+        String tokenValue = "valid-token";
+        Token token = new Token();
+        token.setRevoked(true);
+        when(tokenRepository.findByToken(tokenValue)).thenReturn(Optional.of(token));
+
+        boolean result = tokenService.isTokenRevoked(tokenValue);
+
+        assertThat(result).isTrue();
+        verify(tokenRepository, times(1)).findByToken(tokenValue);
+    }
+
+    @Test
+    void isTokenRevoked_shouldReturnFalse_whenTokenIsNotRevoked() {
+        String tokenValue = "valid-token";
+        Token token = new Token();
+        token.setRevoked(false);
+        when(tokenRepository.findByToken(tokenValue)).thenReturn(Optional.of(token));
+
+        boolean result = tokenService.isTokenRevoked(tokenValue);
+
+        assertThat(result).isFalse();
+        verify(tokenRepository, times(1)).findByToken(tokenValue);
+    }
+
+    @Test
+    void isTokenRevoked_shouldThrowTokenNotFoundException_whenTokenDoesNotExist() {
+        String tokenValue = "invalid-token";
+        when(tokenRepository.findByToken(tokenValue)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> tokenService.isTokenRevoked(tokenValue))
+                .isInstanceOf(TokenNotFoundException.class)
+                .hasMessageContaining("token.not_found");
+
+        verify(tokenRepository, times(1)).findByToken(tokenValue);
+    }
+}
