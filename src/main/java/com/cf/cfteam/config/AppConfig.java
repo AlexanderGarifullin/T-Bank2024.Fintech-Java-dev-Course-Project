addd383f53fb6623349bd09cde739698cac57894
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-12 19:49:21 +0500

    dev/security/ Добавил бины для security

---
+++ src/main/java/com/cf/cfteam/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.cf.cfteam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+
+public class AppConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
