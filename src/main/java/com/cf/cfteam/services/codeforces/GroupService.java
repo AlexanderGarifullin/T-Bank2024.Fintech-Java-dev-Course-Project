a72be82dba52c4d8dfc3291daa0d8f979e161ea5
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-16 14:06:24 +0500

    dev/codeforces/ Добавил сервисы для новых моделей

---
+++ src/main/java/com/cf/cfteam/services/codeforces/GroupService.java
@@ -0,0 +1,63 @@
+package com.cf.cfteam.services.codeforces;
+
+import com.cf.cfteam.exceptions.codeforces.GroupNotFoundException;
+import com.cf.cfteam.exceptions.security.UserNotFoundException;
+import com.cf.cfteam.mappers.codeforces.GroupMapper;
+import com.cf.cfteam.models.entities.codeforces.Group;
+import com.cf.cfteam.models.entities.security.User;
+import com.cf.cfteam.repositories.jpa.codeforces.GroupRepository;
+import com.cf.cfteam.repositories.jpa.security.UserRepository;
+import com.cf.cfteam.transfer.payloads.codeforces.GroupPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GroupService {
+
+    private final GroupRepository groupRepository;
+    private final UserRepository userRepository;
+
+    public List<Group> getAllGroupsByUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+        return groupRepository.findByUser(user);
+    }
+
+    public Group getGroupById(Long groupId) {
+        return groupRepository.findById(groupId)
+                .orElseThrow(() -> new GroupNotFoundException(groupId));
+    }
+
+    public Group addGroupToUser(Long userId, GroupPayload groupPayload) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+
+        Group group = GroupMapper.fromPayloadToEntity(groupPayload, user);
+
+        return groupRepository.save(group);
+    }
+
+    public Group updateGroup(Long groupId, GroupPayload groupPayload) {
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(() -> new GroupNotFoundException(groupId));
+
+        Group updatedGroup = GroupMapper.updateEntityFromPayload(group, groupPayload);
+
+        return groupRepository.save(updatedGroup);
+    }
+
+
+    public void deleteGroup(Long groupId) {
+        groupRepository.deleteById(groupId);
+    }
+
+    public void deleteAllGroupsByUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+        List<Group> groups = groupRepository.findByUser(user);
+        groupRepository.deleteAll(groups);
+    }
+}
---
+++ src/main/java/com/cf/cfteam/services/codeforces/PlayerService.java
@@ -0,0 +1,9 @@
+package com.cf.cfteam.services.codeforces;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlayerService {
+}
