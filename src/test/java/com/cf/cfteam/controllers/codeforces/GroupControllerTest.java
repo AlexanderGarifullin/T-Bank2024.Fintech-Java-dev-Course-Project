061b7f882b28ed62bf80710ea5a85737c88f2dd3
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-16 16:23:55 +0500

    dev/codeforces/test Добавил unit тесты для GroupController

---
+++ src/test/java/com/cf/cfteam/controllers/codeforces/GroupControllerTest.java
@@ -0,0 +1,112 @@
+package com.cf.cfteam.controllers.codeforces;
+
+import com.cf.cfteam.models.entities.codeforces.Group;
+import com.cf.cfteam.services.codeforces.GroupService;
+import com.cf.cfteam.transfer.payloads.codeforces.GroupPayload;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
+
+
+@ActiveProfiles("test")
+class GroupControllerTest {
+
+    @InjectMocks
+    private GroupController groupController;
+
+    @Mock
+    private GroupService groupService;
+
+    private GroupPayload groupPayload;
+    private Group group;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        group = Group.builder()
+                .name("Test Group")
+                .description("Test description")
+                .user(null)
+                .build();
+
+        groupPayload = GroupPayload.builder()
+                .name("Test Group")
+                .description("Test description")
+                .build();
+    }
+
+    @Test
+    void shouldReturnGroupsByUserId() {
+        when(groupService.getAllGroupsByUser(1L)).thenReturn(List.of(group));
+
+        List<Group> groups = groupController.getAllGroupsByUser(1L, null).getBody();
+
+        verify(groupService, times(1)).getAllGroupsByUser(1L);
+
+        assertThat(groups)
+                .isNotNull()
+                .hasSize(1)
+                .contains(group);
+    }
+
+    @Test
+    void shouldReturnGroupById() {
+        when(groupService.getGroupById(1L)).thenReturn(group);
+
+        Group result = groupController.getGroupById(1L, null).getBody();
+
+        verify(groupService, times(1)).getGroupById(1L);
+        assertThat(result).isEqualTo(group);
+    }
+
+    @Test
+    void shouldAddGroupToUser() {
+        when(groupService.addGroupToUser(1L, groupPayload)).thenReturn(group);
+
+
+        Group result = groupController.addGroupToUser(1L, groupPayload, null).getBody();
+
+        verify(groupService, times(1)).addGroupToUser(1L, groupPayload);
+
+        assertThat(result).isEqualTo(group);
+    }
+
+    @Test
+    void shouldUpdateGroup() {
+        when(groupService.updateGroup(1L, groupPayload)).thenReturn(group);
+
+        Group result = groupController.updateGroup(1L, groupPayload, null).getBody();
+
+        verify(groupService, times(1)).updateGroup(1L, groupPayload);
+        assertAll(
+                () -> assertThat(result).isNotNull(),
+                () -> assertThat(result.getName()).isEqualTo(groupPayload.name()),
+                () -> assertThat(result.getDescription()).isEqualTo(groupPayload.description())
+        );
+    }
+
+    @Test
+    void shouldDeleteGroup() {
+        groupController.deleteGroup(1L, null);
+
+        verify(groupService, times(1)).deleteGroup(1L);
+    }
+
+    @Test
+    void shouldDeleteAllGroupsByUser() {
+        groupController.deleteAllGroupsByUser(1L, null);
+
+        verify(groupService, times(1)).deleteAllGroupsByUser(1L);
+    }
+}
+
