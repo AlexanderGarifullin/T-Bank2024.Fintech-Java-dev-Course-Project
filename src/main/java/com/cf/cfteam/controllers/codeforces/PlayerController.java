2b176b60401f9bd19b0ea7a5be99456b15b12411
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-17 04:27:29 +0500

    dev/codeforces/ Добавил контроллеры

--- src/main/java/com/cf/cfteam/controllers/codeforces/PlayerController.java
+++ src/main/java/com/cf/cfteam/controllers/codeforces/PlayerController.java
@@ -1,4 +1,63 @@
 package com.cf.cfteam.controllers.codeforces;
 
+import com.cf.cfteam.models.entities.codeforces.Player;
+import com.cf.cfteam.models.entities.codeforces.Team;
+import com.cf.cfteam.services.codeforces.PlayerService;
+import com.cf.cfteam.transfer.payloads.codeforces.GroupPayload;
+import com.cf.cfteam.transfer.payloads.codeforces.TeamPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/cf/players")
 public class PlayerController {
+    
+    private final PlayerService playerService;
+
+    @GetMapping("/team/{teamId}")
+    public ResponseEntity<List<Player>> getAllPlayersByTeam(@PathVariable Long teamId, Authentication authentication) {
+        List<Player> players = playerService.getAllPlayersByTeam(teamId);
+        return ResponseEntity.ok(players);
+    }
+
+    @GetMapping("/{playerId}")
+    public ResponseEntity<Player> getPlayerById(@PathVariable Long playerId, Authentication authentication) {
+        Player player = playerService.getPlayerById(playerId);
+        return ResponseEntity.ok(player);
+    }
+
+    @PostMapping("/team/{teamId}")
+    public ResponseEntity<Player> addPlayerToTeam(@PathVariable Long teamId, @RequestBody TeamPayload teamPayload,
+                                               Authentication authentication) {
+        Player player = playerService.addPlayerToTeam(teamId, teamPayload);
+        return ResponseEntity.ok(player);
+    }
+
+    @PutMapping("/players/{playerId}/teams/{teamId}")
+    public ResponseEntity<Player> updatePlayerInTeam(@PathVariable Long playerId,
+                                                     @PathVariable Long teamId,
+                                                     @RequestBody GroupPayload groupPayload,
+                                           Authentication authentication) {
+        Player player = playerService.updatePlayer(playerId, teamId, groupPayload);
+        return ResponseEntity.ok(player);
+    }
+
+    @DeleteMapping("/players/{playerId}/teams/{teamId}")
+    public ResponseEntity<Void> deletePlayerFromTeam(@PathVariable Long playerId,
+                                                     @PathVariable Long teamId,
+                                                     Authentication authentication) {
+        playerService.deletePlayerFromTeam(playerId, teamId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/team/{teamId}")
+    public ResponseEntity<Void> deleteAllPlayersFromTeam(@PathVariable Long teamId, Authentication authentication) {
+        playerService.deleteAllPlayersFromTeam(teamId);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/cf/cfteam/controllers/codeforces/TeamController.java
+++ src/main/java/com/cf/cfteam/controllers/codeforces/TeamController.java
@@ -1,4 +1,58 @@
 package com.cf.cfteam.controllers.codeforces;
 
+import com.cf.cfteam.models.entities.codeforces.Team;
+import com.cf.cfteam.services.codeforces.TeamService;
+import com.cf.cfteam.transfer.payloads.codeforces.GroupPayload;
+import com.cf.cfteam.transfer.payloads.codeforces.TeamPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/cf/teams")
 public class TeamController {
+    
+    private final TeamService teamService;
+    
+    @GetMapping("/group/{groupId}")
+    public ResponseEntity<List<Team>> getAllTeamsByGroup(@PathVariable Long groupId, Authentication authentication) {
+        List<Team> teams = teamService.getAllTeamsByGroup(groupId);
+        return ResponseEntity.ok(teams);
+    }
+
+    @GetMapping("/{teamId}")
+    public ResponseEntity<Team> getTeamById(@PathVariable Long teamId, Authentication authentication) {
+        Team team = teamService.getTeamById(teamId);
+        return ResponseEntity.ok(team);
+    }
+
+    @PostMapping("/group/{groupId}")
+    public ResponseEntity<Team> addTeamToGroup(@PathVariable Long groupId, @RequestBody TeamPayload teamPayload,
+                                                Authentication authentication) {
+        Team createdTeam = teamService.addTeamToGroup(groupId, teamPayload);
+        return ResponseEntity.ok(createdTeam);
+    }
+
+    @PutMapping("/{teamId}")
+    public ResponseEntity<Team> updateTeam(@PathVariable Long teamId, @RequestBody GroupPayload groupPayload,
+                                             Authentication authentication) {
+        Team team = teamService.updateTeam(teamId, groupPayload);
+        return ResponseEntity.ok(team);
+    }
+
+    @DeleteMapping("/{teamId}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable Long teamId, Authentication authentication) {
+        teamService.deleteTeam(teamId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/group/{groupId}")
+    public ResponseEntity<Void> deleteAllTeamsByUser(@PathVariable Long groupId, Authentication authentication) {
+        teamService.deleteAllTeamsByGroup(groupId);
+        return ResponseEntity.noContent().build();
+    }
 }
