a2a81a27ec5eda3e3fad4a010bb20ddf690f692a
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-17 04:26:20 +0500

    dev/codeforces/ Добавил мапперы для превращения модели в response

---
+++ src/main/java/com/cf/cfteam/utils/codeforces/mappers/PlayerMapper.java
@@ -0,0 +1,24 @@
+package com.cf.cfteam.utils.codeforces.mappers;
+
+import com.cf.cfteam.models.entities.codeforces.Player;
+import com.cf.cfteam.transfer.responses.codeforces.PlayerResponse;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PlayerMapper {
+
+    private final LoadingCache<String, Double> ratingCache;
+
+    public PlayerResponse fromEntityToResponse(Player player) {
+        return PlayerResponse.builder()
+                .id(player.getId())
+                .login(player.getLogin())
+                .rating(ratingCache.get(player.getLogin()))
+                .build();
+    }
+}
---
+++ src/main/java/com/cf/cfteam/utils/codeforces/mappers/TeamMapper.java
@@ -0,0 +1,39 @@
+package com.cf.cfteam.utils.codeforces.mappers;
+
+import com.cf.cfteam.models.entities.codeforces.Team;
+import com.cf.cfteam.transfer.responses.codeforces.PlayerResponse;
+import com.cf.cfteam.transfer.responses.codeforces.TeamResponse;
+import com.cf.cfteam.utils.codeforces.RatingCalculator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TeamMapper {
+
+    private final PlayerMapper playerMapper;
+
+    public TeamResponse fromEntityToResponse(Team team) {
+        List<PlayerResponse> convertedPlayers = new ArrayList<>();
+        Double teamRating = 0.;
+
+        if (team.getPlayers() != null) {
+            convertedPlayers = team.getPlayers().stream()
+                    .map(playerMapper::fromEntityToResponse)
+                    .toList();
+
+            teamRating = RatingCalculator.aggregateRatings(convertedPlayers);
+        }
+
+        return TeamResponse.builder()
+                .id(team.getId())
+                .name(team.getName())
+                .description(team.getDescription())
+                .players(convertedPlayers)
+                .teamRating(teamRating)
+                .build();
+    }
+}
