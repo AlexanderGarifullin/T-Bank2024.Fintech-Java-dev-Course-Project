10f844c7bce417faa9d80a8ec61b1017337f444d
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-16 14:09:01 +0500

    dev/codeforces/ Добавил маппер для Group

---
+++ src/main/java/com/cf/cfteam/mappers/codeforces/GroupMapper.java
@@ -0,0 +1,22 @@
+package com.cf.cfteam.mappers.codeforces;
+
+import com.cf.cfteam.models.entities.codeforces.Group;
+import com.cf.cfteam.models.entities.security.User;
+import com.cf.cfteam.transfer.payloads.codeforces.GroupPayload;
+
+public class GroupMapper {
+
+    public static Group fromPayloadToEntity(GroupPayload payload, User user) {
+        return Group.builder()
+                .name(payload.name())
+                .description(payload.description())
+                .user(user)
+                .build();
+    }
+
+    public static Group updateEntityFromPayload(Group group, GroupPayload payload) {
+        group.setName(payload.name());
+        group.setDescription(payload.description());
+        return group;
+    }
+}
