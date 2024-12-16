5b5a955b84b29d34e699503174caf4569e9e5567
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-16 14:09:21 +0500

    dev/codeforces/ Добавил исключения для Group

--- src/main/java/com/cf/cfteam/exceptions/security/UserNotFoundException.java
+++ src/main/java/com/cf/cfteam/exceptions/security/UserNotFoundException.java
@@ -6,9 +6,17 @@ import lombok.Getter;
 public class UserNotFoundException extends RuntimeException  {
 
     private final String login;
+    private final Long id;
 
     public UserNotFoundException(String login) {
         super("login.not_found");
         this.login = login;
+        this.id = null;
+    }
+
+    public UserNotFoundException(Long id) {
+        super("id.not_found");
+        this.id = id;
+        this.login = null;
     }
 }
