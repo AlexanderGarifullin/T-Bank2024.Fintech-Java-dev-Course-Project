cdd2495f1aaa5b9ee820a4fb2a3cbd9e001eaafe
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-17 17:11:49 +0500

    dev/codeforces/test Добавил профиль test тесту CodeforcesClientTest

--- src/test/java/com/cf/cfteam/services/client/CodeforcesClientTest.java
+++ src/test/java/com/cf/cfteam/services/client/CodeforcesClientTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -15,6 +16,7 @@ import java.nio.file.Paths;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @WireMockTest(httpPort = 8080)
 class CodeforcesClientTest {
