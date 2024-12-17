718b7aa39a6cda92d4f4feea39389352f8d2b790
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-17 17:04:26 +0500

    dev/codeforces/test Написал тесты для CodeforcesClient

---
+++ src/test/java/com/cf/cfteam/services/client/CodeforcesClientTest.java
@@ -0,0 +1,53 @@
+package com.cf.cfteam.services.client;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+@SpringBootTest
+@WireMockTest(httpPort = 8080)
+class CodeforcesClientTest {
+
+    @Autowired
+    private CodeforcesClient codeforcesClient;
+
+    private static final String MOCK_JSON_PATH = "src/test/resources/codeforces/responses/";
+
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("codeforces.api.base.url", () -> "http://localhost:8080/api");
+    }
+
+    @Test
+    @SneakyThrows
+    void fetchRatingFromApi_ShouldReturnRating() {
+        String responseBody = Files.readString(Paths.get(MOCK_JSON_PATH + "tourist.json"));
+
+        stubFor(get(urlPathEqualTo("/api/user.info"))
+                .withQueryParam("handles", equalTo("tourist"))
+                .withQueryParam("checkHistoricHandles", equalTo("false"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(responseBody)));
+
+        Double rating = codeforcesClient.fetchRatingFromApi("tourist");
+
+        assertThat(rating).isEqualTo(4009);
+    }
+}
