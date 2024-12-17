b44bc08c8570e4c70a07bda4df038acb327da380
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-12-17 14:37:08 +0500

    dev/codeforces/test Написал интеграционные тесты для TeamController

--- src/main/java/com/cf/cfteam/advicers/codeforces/CodeforcesExceptionHandler.java
+++ src/main/java/com/cf/cfteam/advicers/codeforces/CodeforcesExceptionHandler.java
@@ -1,21 +1,56 @@
 package com.cf.cfteam.advicers.codeforces;
 
-import com.cf.cfteam.exceptions.codeforces.GroupNotFoundException;
+import com.cf.cfteam.exceptions.codeforces.*;
 import com.cf.cfteam.utils.ErrorResponseBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class CodeforcesExceptionHandler {
     private static final String ID = "id";
+    private static final String LOGIN = "login";
+    private static final String TEAM_ID = "teamId";
+    private static final String PLAYER_ID = "playerId";
 
     @ExceptionHandler(GroupNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFoundException(GroupNotFoundException ex) {
         return ErrorResponseBuilder.buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND, Map.of(ID, ex.getId()));
     }
 
+    @ExceptionHandler(TeamNotFoundException.class)
+    public ResponseEntity<Object> handleTeamNotFoundException(TeamNotFoundException ex) {
+        return ErrorResponseBuilder.buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND, Map.of(ID, ex.getId()));
+    }
+
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<Object> handlePayerNotFoundException(PlayerNotFoundException ex) {
+        return ErrorResponseBuilder.buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND, Map.of(ID, ex.getId()));
+    }
+
+    @ExceptionHandler(PlayerAlreadyInTeamException.class)
+    public ResponseEntity<Object> handlePlayerAlreadyInTeamException(PlayerAlreadyInTeamException ex) {
+        return ErrorResponseBuilder.buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND, Map.of(LOGIN, ex.getLogin()));
+    }
+
+    @ExceptionHandler(PlayerNotFromTeamException.class)
+    public ResponseEntity<Object> handlePlayerNotFromTeamException(PlayerNotFromTeamException ex) {
+        Map<String, Object> details = new HashMap<>();
+        if (ex.getPlayerId() != null) {
+            details.put(PLAYER_ID, ex.getPlayerId());
+        }
+        if (ex.getTeamId() != null) {
+            details.put(TEAM_ID, ex.getTeamId());
+        }
+
+        return ErrorResponseBuilder.buildErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND,
+                details
+        );
+    }
 }
---
+++ src/test/java/com/cf/cfteam/controllers/codeforces/TeamControllerIntegrationTest.java
@@ -0,0 +1,375 @@
+package com.cf.cfteam.controllers.codeforces;
+
+import com.cf.cfteam.models.entities.codeforces.Team;
+import com.cf.cfteam.repositories.jpa.codeforces.TeamRepository;
+import com.cf.cfteam.transfer.payloads.codeforces.TeamPayload;
+import com.cf.cfteam.transfer.responses.codeforces.TeamResponse;
+import com.cf.cfteam.utils.codeforces.mappers.TeamMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.cf.cfteam.BaseIntegrationTest;
+import com.cf.cfteam.models.entities.codeforces.Group;
+import com.cf.cfteam.models.entities.security.Role;
+import com.cf.cfteam.models.entities.security.User;
+import com.cf.cfteam.repositories.jpa.codeforces.GroupRepository;
+import com.cf.cfteam.repositories.jpa.security.UserRepository;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ActiveProfiles("test")
+public class TeamControllerIntegrationTest extends BaseIntegrationTest {
+
+    private static final String URI = "/api/cf/teams";
+
+    private static final Long BAD_TEAM_ID = 9999L;
+    private static final Long BAD_GROUP_ID = 9999L;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TeamMapper teamMapper;
+
+    private static User user;
+
+    @BeforeEach
+    void init() {
+        user = createUser();
+        user = userRepository.save(user);
+    }
+
+    @AfterEach
+    void postDelete() {
+        deleteUserFromDb(user);
+    }
+
+    @Test
+    @SneakyThrows
+    public void getAllGroupsByUser_notEmpty_success() {
+        Group group = createGroup(user);
+        group = groupRepository.save(group);
+
+        Team team = createTeam(group);
+        team = teamRepository.save(team);
+
+        var mvcResponse = mockMvc.perform(get(URI + "/group/" + group.getId())
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        var teams = objectMapper.readValue(
+                mvcResponse.getContentAsString(),
+                new TypeReference<List<TeamResponse>>() {
+                }
+        );
+
+        assertThat(teams).hasSize(1)
+                .contains(teamMapper.fromEntityToResponse(team));
+
+        deleteTeamFromDb(team);
+        deleteGroupFromDb(group);
+    }
+
+    @Test
+    @SneakyThrows
+    void getAllTeamsByGroup_shouldThrowGroupNotFoundException_whenGroupNotFound() {
+        mockMvc.perform(get(URI + "/group/" + BAD_GROUP_ID)
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.details.id").value(BAD_GROUP_ID),
+                        jsonPath("$.message").value("id.not_found"),
+                        jsonPath("$.error").value("Not Found"),
+                        jsonPath("$.status").value(404)
+                );
+    }
+
+    @Test
+    @SneakyThrows
+    public void getTeamById_notEmpty() {
+        Group group = createGroup(user);
+        group = groupRepository.save(group);
+
+        Team team = createTeam(group);
+        team = teamRepository.save(team);
+
+        var mvcResponse = mockMvc.perform(get(URI + "/" + team.getId())
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        var response = objectMapper.readValue(mvcResponse.getContentAsString(), TeamResponse.class);
+
+        assertThat(response).isEqualTo(teamMapper.fromEntityToResponse(team));
+
+        deleteTeamFromDb(team);
+        deleteGroupFromDb(group);
+    }
+
+    @Test
+    @SneakyThrows
+    void getTeamById_shouldThrowTeamNotFoundException_whenTeamNotFoundE() {
+        mockMvc.perform(get(URI + "/" + BAD_TEAM_ID)
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.details.id").value(BAD_TEAM_ID),
+                        jsonPath("$.message").value("id.not_found"),
+                        jsonPath("$.error").value("Not Found"),
+                        jsonPath("$.status").value(404)
+                );
+    }
+
+    @Test
+    @SneakyThrows
+    public void addTeamToGroup_success() {
+        Group group = createGroup(user);
+        group = groupRepository.save(group);
+
+        var payload = createTeamPayload();
+
+        var mvcResponse = mockMvc.perform(post(URI + "/group/" + group.getId())
+                        .header("Authorization", userBearerToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(payload)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        var response = objectMapper.readValue(mvcResponse.getContentAsString(), TeamResponse.class);
+        var teamFromDb = teamRepository.findById(response.id());
+
+        assertAll(
+                () -> assertThat(teamFromDb).isPresent(),
+                () -> assertThat(response.description()).isEqualTo(payload.description()),
+                () -> assertThat(response.name()).isEqualTo(payload.name())
+        );
+
+        deleteTeamFromDb(teamFromDb.get());
+        deleteGroupFromDb(group);
+    }
+
+    @Test
+    @SneakyThrows
+    void addTeamToGroup_shouldThrowGroupNotFoundException_whenGroupNotFound() {
+        var payload = createTeamPayload();
+
+        mockMvc.perform(post(URI + "/group/" + BAD_GROUP_ID)
+                        .header("Authorization", userBearerToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(payload)))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.details.id").value(BAD_GROUP_ID),
+                        jsonPath("$.message").value("id.not_found"),
+                        jsonPath("$.error").value("Not Found"),
+                        jsonPath("$.status").value(404)
+                );
+    }
+
+    @Test
+    @SneakyThrows
+    public void updateTeam_success() {
+        Group group = createGroup(user);
+        group = groupRepository.save(group);
+
+        var payload = createTeamPayload("new name", "new description");
+
+        Team team = createTeam(group);
+        team = teamRepository.save(team);
+
+
+        var mvcResponse = mockMvc.perform(put(URI + "/" + team.getId())
+                        .header("Authorization", userBearerToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(payload)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+
+        var response = objectMapper.readValue(mvcResponse.getContentAsString(), TeamResponse.class);
+        var teamFromDb = teamRepository.findById(response.id());
+
+        assertAll(
+                () -> assertThat(teamFromDb).isPresent(),
+                () -> assertThat(teamFromDb.get().getDescription()).isEqualTo(payload.description()),
+                () -> assertThat(teamFromDb.get().getName()).isEqualTo(payload.name())
+        );
+
+        deleteTeamFromDb(teamFromDb.get());
+        deleteGroupFromDb(group);
+    }
+
+    @Test
+    @SneakyThrows
+    void updateTeam_shouldThrowTeamNotFoundException_whenTeamNotFound() {
+        var payload = createTeamPayload();
+
+        mockMvc.perform(put(URI + "/" + BAD_TEAM_ID)
+                        .header("Authorization", userBearerToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(payload)))
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.details.id").value(BAD_TEAM_ID),
+                        jsonPath("$.message").value("id.not_found"),
+                        jsonPath("$.error").value("Not Found"),
+                        jsonPath("$.status").value(404)
+                );
+    }
+
+    @Test
+    @SneakyThrows
+    public void deleteTeam_success() {
+        Group group = createGroup(user);
+        group = groupRepository.save(group);
+
+        Team team = createTeam(group);
+        team = teamRepository.save(team);
+
+        mockMvc.perform(delete(URI + "/" + group.getId())
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isNoContent(),
+                        content().string("")
+                );
+
+        var teamFromDb = teamRepository.findById(team.getId());
+
+        assertThat(teamFromDb).isNotPresent();
+
+        deleteGroupFromDb(group);
+    }
+
+    @Test
+    @SneakyThrows
+    public void deleteAllTeamsByGroup_success() {
+        Group group = createGroup(user);
+        group = groupRepository.save(group);
+
+        Team team1 = createTeam(group);
+        team1 = teamRepository.save(team1);
+
+        Team team2 = createTeam(group);
+        team2 = teamRepository.save(team2);
+
+        mockMvc.perform(delete(URI + "/group/" + group.getId())
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isNoContent(),
+                        content().string("")
+                );
+
+        var teamFromDb1 = teamRepository.findById(team1.getId());
+        var teamFromDb2 = teamRepository.findById(team2.getId());
+
+        assertAll(
+                () -> assertThat(teamFromDb1).isNotPresent(),
+                () -> assertThat(teamFromDb2).isNotPresent()
+        );
+
+        var mvcResponse = mockMvc.perform(get(URI + "/group/" + group.getId())
+                        .header("Authorization", userBearerToken))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        var teams = objectMapper.readValue(
+                mvcResponse.getContentAsString(),
+                new TypeReference<List<TeamResponse>>() {
+                }
+        );
+
+        assertThat(teams).isEmpty();
+
+        deleteGroupFromDb(group);
+    }
+
+
+    private TeamPayload createTeamPayload() {
+        return TeamPayload.builder()
+                .name("name")
+                .description("description")
+                .build();
+    }
+
+    private TeamPayload createTeamPayload(String name, String description) {
+        return TeamPayload.builder()
+                .name(name)
+                .description(description)
+                .build();
+    }
+
+    private User createUser() {
+        return User.builder()
+                .name("User name")
+                .login("User login")
+                .hashedPassword("Password")
+                .role(Role.USER)
+                .build();
+    }
+
+    private Group createGroup(User user) {
+        return Group.builder()
+                .name("Test Group")
+                .description("Test description")
+                .user(user)
+                .build();
+    }
+
+    private Team createTeam(Group group) {
+        return Team.builder()
+                .name("team")
+                .description("team description")
+                .group(group)
+                .build();
+    }
+
+
+    private void deleteTeamFromDb(Team team) {
+        teamRepository.delete(team);
+    }
+
+    private void deleteGroupFromDb(Group group) {
+        groupRepository.delete(group);
+    }
+
+    private void deleteUserFromDb(User user) {
+        userRepository.delete(user);
+    }
+}
